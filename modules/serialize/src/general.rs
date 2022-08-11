bc7f2bb038440a45a08cace0c86b513e643961e7
Yiming Li <yimingli.cs@gmail.com>
2022-08-11 09:02:04 +0800

    feat(serialize): 简单实现

--- modules/serialize/src/general.rs
+++ modules/serialize/src/general.rs
@@ -1,6 +1,7 @@
 //! 进行基础序列化工作的系列定义
 
 use crate::Result;
+use serde::{Deserialize, Serialize};
 
 /// 可序列化类型的标注 trait
 /// TODO: 这个 Trait 是用来隐藏不同的序列化实现的，当然嫌麻烦也可以去掉。
@@ -14,9 +15,7 @@ use crate::Result;
 ///       参考资料：
 ///       - https://doc.rust-lang.org/beta/unstable-book/language-features/auto-traits.html
 pub trait HostcallValue {}
-
-// FIXME: 此处仅供暂时 Mock，实际上 Sized 是一个过于宽泛的约束了
-impl<T> HostcallValue for T where T: Sized {}
+impl<'a, T> HostcallValue for T where T: Serialize + Deserialize<'a> {}
 
 /// 对序列化所需的内部数据结构进行封装 FIXME: 如果不需要可以不含任何字段
 pub struct SerializeCtx;
@@ -39,9 +38,11 @@ impl SerializeCtx {
     /// let serialized = ctx.serialize::<_>(&value).unwrap(); // 返回值持有了序列化后数据的所有权
     /// let bytes: &[u8] = serialized.as_ref();
     /// ```
-    pub fn serialize<T: HostcallValue>(&self, value: &T) -> Result<impl AsRef<[u8]>> {
-        // TODO: 此处仅为了返回类型推导方便，实际上可以返回任何能转换为 &[u8] 的类型
-        Ok(vec![0u8; 100])
+    pub fn serialize<'a, T: Serialize>(&self, value: &T) -> Result<impl AsRef<[u8]>> {
+        match serde_json::to_string(value) {
+            Ok(json) => Ok(json.into_bytes()),
+            Err(error) => Err(error.into()),
+        }
     }
 
     /// 对所给的二进制数据进行反序列化
@@ -62,10 +63,14 @@ impl SerializeCtx {
     /// let actual: &str = ctx.deserialize::<&str>(bytes).unwrap().into();
     /// assert_eq!(expected, actual);
     /// ```
-    pub fn deserialize<'a, 'b, T>(&'a self, bytes: &'b [u8]) -> Result<impl Into<&'b T>>
-        where T: HostcallValue + 'b,
+    pub fn deserialize<'a, 'b, T>(&'a self, bytes: &'b [u8]) -> Result<T>
+    where
+        T: Deserialize<'b>,
     {
-        todo!() as Result<&'b T>
+        match serde_json::from_str::<T>(std::str::from_utf8(bytes)?) {
+            Ok(value) => Ok(value),
+            Err(error) => Err(error.into()),
+        }
     }
 }
 
@@ -73,5 +78,22 @@ impl SerializeCtx {
 mod tests {
     use super::*;
 
-// TODO: 有时间的话应该添加测试用例
+    #[test]
+    fn should_serialize() {
+        let ctx = SerializeCtx::new();
+        let value = "hello world";
+        let serialized = ctx.serialize::<_>(&value).unwrap();
+        let bytes: &[u8] = serialized.as_ref();
+        let ans: [u8; 13] = [34, 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 34];
+        assert_eq!(bytes, ans);
+    }
+
+    #[test]
+    fn should_deserialize() {
+        let ctx = SerializeCtx::new();
+        let bytes: [u8; 13] = [34, 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 34];
+        let s: String = ctx.deserialize(&bytes).unwrap();
+        let ans = "hello world";
+        assert_eq!(s, ans);
+    }
 }
