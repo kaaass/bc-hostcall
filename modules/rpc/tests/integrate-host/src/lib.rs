08e8aed937dcb9803ba82e7e7dcbd3aa3b4a4b65
KAAAsS <admin@kaaass.net>
2022-08-12 00:49:08 +0800

    test(rpc): 测试增加 WASM 标准流输出

--- modules/rpc/tests/integrate-host/src/lib.rs
+++ modules/rpc/tests/integrate-host/src/lib.rs
@@ -40,7 +40,9 @@ mod utils {
         wasmtime_wasi::add_to_linker(&mut linker, |s| s).unwrap();
 
         // 创建 WASI 上下文
-        let wasi = WasiCtxBuilder::new().build();
+        let wasi = WasiCtxBuilder::new()
+            .inherit_stdio()
+            .build();
         let store = Store::new(&engine, wasi);
 
         // 创建 Module 并进行实例化
