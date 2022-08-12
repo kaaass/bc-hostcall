0d2f1f5e1fd29053db22cf4132d7ea4c7a9f001e
KAAAsS <admin@kaaass.net>
2022-08-13 00:04:09 +0800

    feat(low-level): 切换编译选项，允许非 wasm32 架构暴露更多接口

--- modules/low-level/src/wasm.rs
+++ modules/low-level/src/wasm.rs
@@ -1,5 +1,6 @@
 //! 本模块用于提供在 WASM 中进行信息传送的低层接口
 
+#[cfg(target_arch = "wasm32")]
 use std::alloc::{self, Layout};
 
 use crate::Result;
@@ -15,17 +16,20 @@ use crate::Result;
 /// send_message_to_host("hello host".as_bytes()).unwrap();
 /// ```
 ///
+#[allow(unused_variables)]
 pub fn send_message_to_host(msg: &[u8]) -> Result<()> {
-    #[link(wasm_import_module = "__bc_low_level")]
-    extern "C" {
-        #[link_name = "receive_message_from_wasm"]
-        fn host_message_handler(msg: *const u8, msg_len: usize);
-    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        #[link(wasm_import_module = "__bc_low_level")]
+        extern "C" {
+            #[link_name = "receive_message_from_wasm"]
+            fn host_message_handler(msg: *const u8, msg_len: usize);
+        }
 
-    unsafe {
-        host_message_handler(msg.as_ptr(), msg.len());
+        unsafe {
+            host_message_handler(msg.as_ptr(), msg.len());
+        }
     }
-
     Ok(())
 }
 
@@ -47,6 +51,7 @@ pub fn send_message_to_host(msg: &[u8]) -> Result<()> {
 #[macro_export]
 macro_rules! set_message_callback {
     ($cb:ident) => {
+        #[cfg(target_arch = "wasm32")]
         #[no_mangle]
         pub extern "C" fn __bc_low_level_host_message_handler(msg: *const u8, msg_len: usize) {
             let msg = unsafe {
@@ -62,6 +67,7 @@ macro_rules! set_message_callback {
 /// 本函数是开源项目 bytecodealliance/wit-bindgen 的一部分，遵照 Apache License 协议引入
 ///
 #[doc(hidden)]
+#[cfg(target_arch = "wasm32")]
 #[no_mangle]
 unsafe extern "C" fn canonical_abi_realloc(
     old_ptr: *mut u8,
@@ -91,6 +97,7 @@ unsafe extern "C" fn canonical_abi_realloc(
 /// 本函数是开源项目 bytecodealliance/wit-bindgen 的一部分，遵照 Apache License 协议引入
 ///
 #[doc(hidden)]
+#[cfg(target_arch = "wasm32")]
 #[no_mangle]
 pub unsafe extern "C" fn canonical_abi_free(ptr: *mut u8, len: usize, align: usize) {
     if len == 0 {
